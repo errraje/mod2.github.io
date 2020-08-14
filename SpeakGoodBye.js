38b68ba3099e129a0630800052c4325918390dbe
errraje <67918154+errraje@users.noreply.github.com>
2020-08-14 18:38:02 +0530

    Update SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,8 +1,7 @@
 (function(window)
 {
 var speakWord = "Good Bye";
-var byeSpeaker=function(name)
-{
+var byeSpeaker=function(name){
 	console.log(speakWord+" "+name);
 }
 window.byeSpeaker=byeSpeaker;
